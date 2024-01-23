d5446faf7e2e95c59c370981d5d4dea0fb333dc1
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-01-22 20:26:59 -0700

    removed temp log

--- logging_config.py
+++ logging_config.py
@@ -1,12 +1,12 @@
 import logging
 
 def start_loggers():
-  logger = logging.getLogger('error_logger')
-  logger.setLevel(logging.ERROR)
+  error_logger = logging.getLogger('error_logger')
+  error_logger.setLevel(logging.ERROR)
   error_handler = logging.FileHandler('error.log')
   error_formatter = logging.Formatter('%(asctime)s - %(name)s - ERROR - %(message)s')
   error_handler.setFormatter(error_formatter)
-  logger.addHandler(error_handler)
+  error_logger.addHandler(error_handler)
 
   info_logger = logging.getLogger('info_logger')
   info_logger.setLevel(logging.INFO)
