46619b5b05b334f7e1278de18bcda385ab54bd4b
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-01-22 20:28:00 -0700

    fixed dictionary key deletion in cleanup daemon, pointing to folder_path instead of folder_name

--- cleanup.py
+++ cleanup.py
@@ -13,11 +13,11 @@ def cleanup_directory(UPLOAD_FOLDER):
     for folder_name in os.listdir(UPLOAD_FOLDER):
       folder_path = os.path.join(UPLOAD_FOLDER, folder_name)
       if os.path.isdir(folder_path):
-        creation_time = os.path.getctime(folder_path)
         try:
           shutil.rmtree(folder_path)
-          del training_status[folder_path]
+          del training_status[folder_name]
         except Exception as e:
+          creation_time = os.path.getctime(folder_path)
           time_passed = now - creation_time
           if time_passed > 3600:
             length_time = f"{time_passed//60} minutes {round(time_passed%60)}seconds"
