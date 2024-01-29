51c6d8ec4a96e628f4155df13492581f362ad9d8
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-01-28 22:07:40 -0700

    fixed bug where uninitialized verison of client being passed

--- finetune/openai_client.py
+++ finetune/openai_client.py
@@ -1,8 +1,11 @@
 from openai import OpenAI
 
-
 client = None
 
 def set_client(api_key:str):
   global client
+  api_key = api_key
   client = OpenAI()
+
+def get_client():
+  return client
--- finetune/training_management.py
+++ finetune/training_management.py
@@ -3,7 +3,7 @@ import time
 
 from file_handling import read_text_file, write_jsonl_file
 from finetune.chunking import split_into_chunks
-from finetune.openai_client import client, set_client
+from finetune.openai_client import get_client, set_client
 from finetune.shared_resources import training_status
 
 
@@ -29,24 +29,24 @@ def fine_tune(folder_name: str, user_folder: str):
   Returns:
     None
   """
-
+  client = get_client()
   fine_tune_file = os.path.join(folder_name, "fine_tune.jsonl")
 
   JSONL_file = client.files.create(
     file = open(fine_tune_file, "rb"),
-    purpose = "processing"
+    purpose = "fine-tune"
   )
 
   training_status[user_folder] = f"Uploaded file id {JSONL_file.id}"
   message = "processing"
   while True:
-    upload_file = client.fine_tuning.jobs.retrieve(id=JSONL_file.id)
+    upload_file = client.fine_tuning.jobs.retrieve(JSONL_file.id)
     psuedo_animation(folder_name, message)
-    if upload_file.status == "processed":
+    if upload_file.status in ["queued", "running"]:
       training_status[user_folder] = "File processed"
       break
 
-  fine_tune_job = client.fine_tuning.jobs.create(training_file=JSONL_file.id, model="gpt-3.5-turbo-1105")
+  fine_tune_job = client.fine_tuning.jobs.create(training_file=JSONL_file.id, model="gpt-3.5-turbo")
   message = "Fine-tuning"
   while True:
     fine_tune_info = client.fine_tuning.jobs.retrieve(id=fine_tune_job.id)
@@ -55,7 +55,7 @@ def fine_tune(folder_name: str, user_folder: str):
       training_status[user_folder] = (
         f"{fine_tune_info.status}"
         f"Fine-tuned model info {fine_tune_info}"
-        f"Model id {fine_tune_info.fine_tuned_model}"
+        f"Model id {fine_tune_info.id }"
       )
       break
 
