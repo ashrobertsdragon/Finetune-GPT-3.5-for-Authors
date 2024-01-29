e28d4c420a2229ea43adae04e360a57b936d3d69
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-01-29 12:31:13 -0700

    removed depreciated fine tuning commands

--- finetune/training_management.py
+++ finetune/training_management.py
@@ -37,20 +37,11 @@ def fine_tune(folder_name: str, user_folder: str):
     purpose = "fine-tune"
   )
 
-  training_status[user_folder] = f"Uploaded file id {JSONL_file.id}"
-  message = "processing"
-  while True:
-    upload_file = client.fine_tuning.jobs.retrieve(JSONL_file.id)
-    psuedo_animation(folder_name, message)
-    if upload_file.status in ["queued", "running"]:
-      training_status[user_folder] = "File processed"
-      break
 
   fine_tune_job = client.fine_tuning.jobs.create(training_file=JSONL_file.id, model="gpt-3.5-turbo")
-  message = "Fine-tuning"
   while True:
-    fine_tune_info = client.fine_tuning.jobs.retrieve(id=fine_tune_job.id)
-    psuedo_animation(message)
+    fine_tune_info = client.fine_tuning.jobs.retrieve(fine_tune_job.id)
+    psuedo_animation(user_folder, "Finetuning")
     if fine_tune_info.status == "succeeded":
       training_status[user_folder] = (
         f"{fine_tune_info.status}"
