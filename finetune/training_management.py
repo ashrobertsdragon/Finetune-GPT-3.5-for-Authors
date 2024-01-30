d8cd7b94fa45e6168065e4a1cc54077c1dfa99ac
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-01-30 16:17:58 -0700

    added error handling to finetuning API calling function

--- finetune/training_management.py
+++ finetune/training_management.py
@@ -1,11 +1,20 @@
 import os
 import time
 
+import openai
+
 from file_handling import read_text_file, write_jsonl_file
 from finetune.chunking import split_into_chunks
 from finetune.openai_client import get_client, set_client
 from finetune.shared_resources import training_status
 
+
+def email_admin(e):
+  from dotenv import load_dotenv
+  from send_email import send_mail
+
+  load_dotenv()
+  send_mail(name="Admin", user_email = os.environ("mail_username"), message = "fIrrecoverable error from OpenAI: {e}")
 
 def psuedo_animation(user_folder: str, message: str):
   """Simulate a animation by updating the training status with a message and
@@ -15,7 +24,7 @@ def psuedo_animation(user_folder: str, message: str):
     training_status[user_folder] = f"{message}{'.' * i}"
     time.sleep(1)
 
-def fine_tune(folder_name: str, user_folder: str):
+def fine_tune(folder_name: str, user_folder: str, retry_count: int = 0):
   """
   Fine-tunes a language model using the specified data.
 
@@ -29,26 +38,43 @@ def fine_tune(folder_name: str, user_folder: str):
   Returns:
     None
   """
+  if retry_count:
+    time.sleep(retry_count**retry_count)
+
   client = get_client()
   fine_tune_file = os.path.join(folder_name, "fine_tune.jsonl")
 
-  JSONL_file = client.files.create(
-    file = open(fine_tune_file, "rb"),
-    purpose = "fine-tune"
-  )
+  try:
+    JSONL_file = client.files.create(
+      file = open(fine_tune_file, "rb"),
+      purpose = "fine-tune"
+    )
 
 
-  fine_tune_job = client.fine_tuning.jobs.create(training_file=JSONL_file.id, model="gpt-3.5-turbo")
-  while True:
-    fine_tune_info = client.fine_tuning.jobs.retrieve(fine_tune_job.id)
-    psuedo_animation(user_folder, "Finetuning")
-    if fine_tune_info.status == "succeeded":
-      training_status[user_folder] = (
-        f"{fine_tune_info.status}"
-        f"Fine-tuned model info {fine_tune_info}"
-        f"Model id {fine_tune_info.id }"
-      )
-      break
+    fine_tune_job = client.fine_tuning.jobs.create(training_file=JSONL_file.id, model="gpt-3.5-turbo")
+    while True:
+      fine_tune_info = client.fine_tuning.jobs.retrieve(fine_tune_job.id)
+      psuedo_animation(user_folder, "Finetuning")
+      if fine_tune_info.status == "succeeded":
+        training_status[user_folder] = (
+          f"{fine_tune_info.status}"
+          f"Fine-tuned model info {fine_tune_info}"
+          f"Model id {fine_tune_info.id }"
+        )
+        break
+  except openai.NotFoundError as e:
+    email_admin(e)
+    return
+  except (openai.AuthenticationError, openai.BadRequestError, openai.PermissionDeniedError, openai.RateLimitError)as e:
+    training_status[user_folder] = e.message
+    return
+  except (openai.APIConnectionError, openai.APITimeoutError, openai.ConflictError, openai.InternalServerError, openai.UnprocessableEntityError) as e:
+    retry_count += 1
+    if retry_count > 3:
+      training_status[user_folder] = "A critical error has occured. The administrator has been contacted. Sorry for the inconvience"
+      email_admin(e)
+      return
+    fine_tune(folder_name, user_folder, retry_count)
 
 def process_files(folder_name: str, role: str, chunk_type: str, user_folder: str) -> str:
   """
@@ -105,7 +131,7 @@ def train(folder_name: str, role: str, user_key: str, chunk_type: str):
   training_status[user_folder] = "Processing files"
   set_client(user_key)
   fine_tune_path = process_files(folder_name, role, chunk_type, user_folder)
-  fine_tune(folder_name, user_folder)
+  #fine_tune(folder_name, user_folder)
   download_path = os.path.relpath(fine_tune_path, start=os.path.join("app", "upload_folder"))
   training_status[user_folder] = f"Download <a href='/download/{download_path}'>JSONL file here</a>."
   del user_key
