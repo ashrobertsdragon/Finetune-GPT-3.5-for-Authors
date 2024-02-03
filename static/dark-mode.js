a04f45621c9c059d5b7ee04d9614d0b9d2917a15
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-02-02 17:10:22 -0700

    started work on landing page

--- static/dark-mode.js
+++ static/dark-mode.js
@@ -2,13 +2,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const toggleSwitch = document.getElementById('darkModeToggle');
   const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-  function updateLogo() {
+  function updateModeElements() {
     if (document.body.classList.contains("dark-mode")) {
       logo.src = 'static/logo-dark.png';
       endError.src = "static/alert-dark.png";
+      toggleLabel.textContent = "Light mode";
     } else {
       logo.src = 'static/logo.png';
       endError.src = "static/alert-light.png"
+      toggleLabel.textContent = "Dark mode";
     }
   }
 
@@ -21,12 +23,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.body.classList.add("light-mode");
     document.body.classList.remove("dark-mode");
   }
-  updateLogo();
+  updateModeElements();
 
   // Listen for toggle switch
   toggleSwitch.addEventListener('change', () => {
     document.body.classList.toggle("dark-mode");
     document.body.classList.toggle("light-mode");
-    updateLogo();
+    updateModeElements();
   });
 })
