98175370578a065e08648d17d0878b414e983cc3
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-02-13 07:51:21 -0700

    Added encoding checks and reencoding to UTF-8

--- file_handling.py
+++ file_handling.py
@@ -1,12 +1,38 @@
 import json
+import os
 
-def is_utf8(file_path: str) -> bool:
+import chardet
+
+def is_encoding(file_path: str, encoding: str) -> bool:
   try:
-    with open(file_path, "r", encoding="utf-8") as file:
-      file.read()
+    with open(file_path, "r", encoding=encoding) as f:
+      f.read()
     return True
   except UnicodeDecodeError:
     return False
+
+def auto_detect_encoding(file_path: str) -> str:
+  with open(file_path, 'rb') as f:
+    raw_data = f.read()
+  return chardet.detect(raw_data)['encoding']
+
+def manual_detect_encoding(file_path: str) -> str:
+  for encoding in ["iso-8859-1", "windows-1252", "utf-16", "utf-32", "iso-8859-2", "iso-8859-5", "iso-8859-6", "iso-8859-7", "iso-8859-9"]:
+    if is_encoding(file_path, encoding):
+      return encoding
+  return ""
+
+def recode_text(original_file_path: str) -> str:
+  base_name, ext = os.path.splitext(original_file_path)
+  recoded_file_path = f"{base_name}-recoded{ext}"
+  detected_encoding = auto_detect_encoding(original_file_path) or manual_detect_encoding(original_file_path)
+  with open(original_file_path, "r", encoding=detected_encoding or "utf-8", errors="ignore") as f:
+    content = f.read()
+  with open(recoded_file_path, "w", encoding="utf-8") as f:
+    f.write(content)
+
+
+
   
 def read_text_file(file_path: str) -> str:
   with open(file_path, "r") as f:
