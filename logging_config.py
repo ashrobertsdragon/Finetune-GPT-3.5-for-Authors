b83511c9bd3eeda67352724bc62ec5205a0c327a
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-03-01 13:09:43 -0700

    updated logging initator to use Google's Cloud Logging

--- logging_config.py
+++ logging_config.py
@@ -1,16 +1,19 @@
 import logging
 
-def start_loggers():
-  error_logger = logging.getLogger('error_logger')
-  error_logger.setLevel(logging.ERROR)
-  error_handler = logging.FileHandler('error.log')
-  error_formatter = logging.Formatter('%(asctime)s - %(name)s - ERROR - %(message)s')
-  error_handler.setFormatter(error_formatter)
-  error_logger.addHandler(error_handler)
+from google.cloud import logging as cloud_logging
 
-  info_logger = logging.getLogger('info_logger')
-  info_logger.setLevel(logging.INFO)
-  info_handler = logging.FileHandler('info.log')
-  info_formatter = logging.Formatter('%(asctime)s - %(name)s - INFO - %(message)s')
-  info_handler.setFormatter(info_formatter)
-  info_logger.addHandler(info_handler)
+def start_loggers():
+    "Starts loggers for error and info logs"
+
+    logging_client = cloud_logging.Client()
+    cloud_handler = logging_client.get_default_handler()
+
+    error_logger = logging.getLogger("error_logger")
+    error_logger.setLevel(logging.ERROR)
+    error_logger.addHandler(cloud_handler)
+    error_extra = {"labels": {"type": "error", "application": "your-application-name"}}
+
+    info_logger = logging.getLogger("info_logger")
+    info_logger.setLevel(logging.INFO)
+    info_logger.addHandler(cloud_handler)
+    info_extra = {"labels": {"type": "info", "application": "your-application-name"}}
