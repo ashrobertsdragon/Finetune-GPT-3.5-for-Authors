4ef1579b9f3cd67d8db131bb2b980999d260bbce
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-03-09 07:20:25 -0700

    removed code from bad tutorial for supabase auth and swaped out correct supabase client initialization

--- src/supabase_client.py
+++ src/supabase_client.py
@@ -1,22 +1,6 @@
 import os
-from flask import g
-from werkzeug.local import LocalProxy
-from supabase.client import Client, ClientOptions
-from flask_storage import FlaskSessionStorage
+from supabase import create_client, Client
 
-url = os.environ.get("SUPABASE_URL", "")
-key = os.environ.get("SUPABASE_KEY", "")
-
-def get_supabase() -> Client:
-    if "supabase" not in g:
-        g.supabase = Client(
-            url,
-            key,
-            options=ClientOptions(
-                storage=FlaskSessionStorage(),
-                flow_type="pkce"
-            ),
-        )
-    return g.supabase
-
-supabase: Client = LocalProxy(get_supabase)
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
