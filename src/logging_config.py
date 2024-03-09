947cbf829b5bbfeeb74a74bca5e628aa2525064a
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-03-09 10:21:32 -0700

    dummy logging file

---
+++ src/logging_config.py
@@ -0,0 +1,17 @@
+import logging
+
+from google.cloud import logging as cloud_logging
+
+def start_loggers():
+    "Starts loggers for error and info logs"
+
+    logging_client = cloud_logging.Client()
+    cloud_handler = logging_client.get_default_handler()
+
+    error_logger = logging.getLogger("error_logger")
+    error_logger.setLevel(logging.ERROR)
+    error_logger.addHandler(cloud_handler)
+
+    info_logger = logging.getLogger("info_logger")
+    info_logger.setLevel(logging.INFO)
+    info_logger.addHandler(cloud_handler)
