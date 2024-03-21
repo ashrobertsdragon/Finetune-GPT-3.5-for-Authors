f2dec1b2877ab630af7afb7a7593babe0572acc7
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-03-20 20:56:09 -0600

    using service role instead of public key to bypass RLS because you can't use row level authentication before a user exists

--- src/supabase.py
+++ src/supabase.py
@@ -5,7 +5,7 @@ from supabase import create_client, Client
 from .error_handling import email_admin
 
 url: str = config("SUPABASE_URL")
-key: str = config("SUPABASE_KEY")
+key: str = config("SUPABASE_SERVICE_ROLE")
 supabase: Client = create_client(url, key)
 
 def update_db():
