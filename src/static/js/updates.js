208f0ac3a807d61adfe61b2dc9644893f689b46f
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-04-09 00:10:07 -0600

    moved updates.js into updates.html template and conditional checks for elements to avoid errors and unecessary javascript loading

--- src/static/js/updates.js
+++ src/static/js/updates.js
@@ -1,7 +1,9 @@
 const closeButton = document.getElementById("close");
-const parentDiv = closeButton.parentElement;
+const parentDiv = document.getElementById("parentDiv");
 
-closeButton.addEventListener("click", function() {
-  parentDiv.classList.remove("shown");
-  parentDiv.classList.add("hidden");
-});
+if (closeButton && parentDiv) {
+  closeButton.addEventListener("click", function() {
+    parentDiv.classList.remove("shown");
+    parentDiv.classList.add("hidden");
+  });
+}
