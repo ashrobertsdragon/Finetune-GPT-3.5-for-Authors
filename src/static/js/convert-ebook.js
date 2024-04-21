493a8b265a978cf1a09e2768081babdf77777bfc
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-04-21 12:57:12 -0600

    when converting inline script to file, never collected data needed for script. that is fixed now

--- src/static/js/convert-ebook.js
+++ src/static/js/convert-ebook.js
@@ -1,34 +1,42 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const form = document.getElementById('conversionForm');
-  const loadingMessage = document.getElementById('loadingMessage');
-  const titleField = form.querySelector('input[name="title"]');
-  const fileUploader = form.querySelector('input[name="ebook"]');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const form = document.getElementById("conversionForm");
+  const loadingMessage = document.getElementById("loadingMessage");
+  const titleField = form.querySelector("input[name='title']");
+  const fileUploader = form.querySelector("input[name='ebook']");
+  const formActionUrl = form.action;
 
-  form.addEventListener('submit', (e) => {
-    loadingMessage.style.display = 'block';
+  form.addEventListener("submit", (e) => {
+    loadingMessage.style.display = "block";
 
-    // Clear specific fields after a short delay to allow form submission
-    setTimeout(() => {
-      titleField.value = '';
-      fileUploader.value = '';
-      loadingMessage.style.display = 'none';
-    }, 100); // Adjust the delay as needed
-  });
+    const formData = new FormData(form);
+    fetch(formActionUrl, {
+      method: "POST",
+      body: formData
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(data => {
+    })
+    .catch(async (errorPromise) => {
+      const error = await errorPromise;
+      console.error("Error:", error);
+      alert(error.error);
+    })
+    .finally(() => {
+      titleField.value = "";
+      fileUploader.value = "";
+      loadingMessage.style.display = "none";
+    })
+  })
 });
-fetch('/convert-ebook', {
-  method: 'POST',
-  body: formData
-})
-.then(response => {
-  if (!response.ok) {
-    throw response.json();
-  }
-  return response.json();
-})
-.then(data => {
-})
-.catch(async (errorPromise) => {
-  const error = await errorPromise;
-  console.error('Error:', error);
-  alert(error.error);
-});
+
+
+
+
+
+
+;
