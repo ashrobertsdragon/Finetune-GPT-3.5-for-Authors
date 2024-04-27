f4338fbfc8183f6abe7ab7b49b80c6a75c01b1ef
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-04-27 10:52:23 -0600

    added new modal to closeModal.js

--- src/static/js/closeModal.js
+++ src/static/js/closeModal.js
@@ -2,12 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeIcon = document.querySelector('.close-icon');
   const flashModal = document.querySelector('.flash-modal');
   const errorModal = document.querySelector('.error-modal');
+  const messageModal = document.querySelector('.message-modal');
 
-  let modal = flashModal || errorModal;
+  let modal = flashModal || errorModal || messageModal;
 
   if (closeIcon && modal) {
     closeIcon.addEventListener('click', () => {
-      modal.style.display = 'none';
+      modal.classList.remove("shown");
+      modal.classList.add("hidden");
     });
   };
 });
