2006b292d150814c39dd2b07e2d90852aa9dd177
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-04-29 15:42:10 -0600

    fixed error where closeIcon event listener attaching to first closeIcon when two on same page, even if that was not the active modal

--- src/static/js/modal.js
+++ src/static/js/modal.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const closeIcon = document.querySelector('.close-icon');
+
   const flashModal = document.querySelector('.flash-modal');
   const errorModal = document.querySelector('.error-modal');
   const messageModal = document.querySelector('.message-modal');
 
   let modal = flashModal || errorModal || messageModal;
 
-  if (closeIcon && modal) {
+  if (modal) {
+    const closeIcon = modal.querySelector('.close-icon');
     closeIcon.addEventListener('click', () => {
       modal.classList.remove("shown");
       modal.classList.add("hidden");
