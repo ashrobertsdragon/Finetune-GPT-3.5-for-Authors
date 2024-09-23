24837ecaa62f1e24bb86dba22988c3596133b05c
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-09-22 22:09:41 -0600

    Split controller logic out of views into separate functions

--- src/prosepal/free/views.py
+++ src/prosepal/free/views.py
@@ -1,10 +1,15 @@
-import os
+from pathlib import Path
 
-from flask import Blueprint, current_app, jsonify, render_template, request
-from prosepal.free.utils import is_encoding, make_folder
-from prosepal.utils import random_str
+from flask import Blueprint, jsonify, render_template, request, send_file
 
-from .forms import EbookConversionForm, FineTuneForm
+# from prosepal.exceptions import APIError
+from prosepal.free import utils
+
+from .controller import (
+    process_convert_ebook_submission,
+    process_finetune_submission,
+)
+from .forms import EbookConversionForm, FineTuneForm, JSONLConversionForm
 
 free_app = Blueprint("free", __name__)
 
@@ -13,53 +18,33 @@ def training_status():
     pass  # dummy function till API is implemented
 
 
+@free_app.route("/create-jsonl", methods=["GET", "POST"])
+def create_jsonl():
+    form = JSONLConversionForm()
+    if form.validate_on_submit():
+        if uploaded_file := form.csv.data:
+            filename: str = Path(uploaded_file.filename).stem
+            return_file = utils.call_jsonl_api(uploaded_file)
+            return send_file(
+                return_file,
+                as_attachment=True,
+                download_name=f"{filename}.jsonl",
+                mimetype="application/octet-stream",
+            )
+    return render_template("free/create-jsonl.html", form=form)
+
+
 @free_app.route("/convert-ebook", methods=["GET", "POST"])
 def convert_ebook():
     form = EbookConversionForm()
-    appl = "application"
-    supported_mimetypes = [
-        f"{appl}/epub+zip",
-        f"{appl}/pdf",
-        f"{appl}/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        "text/plain",
-    ]
-
     if form.validate_on_submit():
-        uploaded_file = form.ebook.data
-        title = form.title.data
-        author = form.author.data
+        book_name, download_path = process_convert_ebook_submission(form)
+        return send_file(
+            path_or_file=download_path,
+            as_attachment="True",
+            download_name=book_name,
+        )
 
-        if uploaded_file:
-            if uploaded_file.mimetype not in supported_mimetypes:
-                return jsonify({"error": "Unsupported file type"}), 400
-
-            folder_name, _ = make_folder()
-            file_path = os.path.join(folder_name, uploaded_file.filename)
-            uploaded_file.save(file_path)
-            if uploaded_file.mimetype == "text/plain" and not is_encoding(
-                file_path, "utf-8"
-            ):
-                current_app.logger.error(f"{file_path} is not UTF-8")
-                error_msg = (
-                    "Not correct kind of text file. " "Please resave as UTF-8"
-                )
-                return jsonify({"error": error_msg}), 400
-
-            metadata = {"title": title, "author": author}
-            print(metadata)  # All this is changing
-            """
-            book_name, psuedopath = convert_file(file_path, metadata)
-
-            with NamedTemporaryFile() as temp_file:
-                blob = DOWNLOAD_FOLDER.blob(psuedopath)
-                blob.download_to_filename(temp_file.name)
-
-                return send_file(
-                    path_or_file=temp_file.name,
-                    as_attachment="True",
-                    download_name=book_name
-                )
-"""
     return render_template("free/convert-ebook.html", form=form)
 
 
@@ -67,58 +52,7 @@ def convert_ebook():
 def finetune():
     form = FineTuneForm()
     if form.validate_on_submit():
-        role = form.role.data
-        chunk_type = form.chunk_type.data
-        user_key = form.user_key.data
-
-        # Validate user key
-        if not (user_key.startswith("sk-") and 50 < len(user_key) < 60):
-            current_app.logger.error("invalid key")
-            return jsonify({"error": "Invalid user key"}), 400
-
-        folder_name, user_folder = make_folder()
-
-        files = request.files.getlist("file")
-        if not files:
-            return jsonify({"error": "No files uploaded"}), 400
-        for file in files:
-            if (
-                file
-                and file.filename.endswith(("txt", "text"))
-                and file.mimetype == "text/plain"
-            ):
-                random_filename = f"{random_str()}.txt"
-                file_path = os.path.join(folder_name, random_filename)
-                file.save(file_path)
-
-                file_size = os.path.getsize(file_path)
-                min_size = 1024  # 1 KB
-                max_size = 1024 * 1024  # 1 MB
-                if file_size < min_size or file_size > max_size:
-                    os.remove(file_path)
-                    current_app.logger.error(
-                        f"{file_path} has an invalid size"
-                    )
-                    return jsonify({"error": "Invalid file size"}), 400
-
-                if not is_encoding(file_path, "utf-8"):
-                    os.remove(file_path)
-                    current_app.logger.error(f"{file_path} is not UTF-8")
-                    error_msg = (
-                        "Not correct kind of text file. "
-                        "Please resave as UTF-8"
-                    )
-                    return jsonify({"error": error_msg}), 400
-            else:
-                current_app.logger.error("File is not text file")
-
-        def call_api(folder_name, role, user_key, chunk_type, user_folder):
-            pass  # placeholder
-
-        call_api(folder_name, role, user_key, chunk_type, user_folder)
-
-        return jsonify({"success": True, "user_folder": user_folder})
-
+        return process_finetune_submission(form)
     return render_template("free/finetune.html", form=form)
 
 
